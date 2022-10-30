fcf0908b49e9802ce15caafd63122511d36a75dd
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2022-10-30 02:38:49 +0100

    This is my first commit i only worked on structuring with no css included

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+
+function App() {
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <img alt=""/>
+            <h2>Annette black </h2>
+            {/* icon */}
+          </div>
+          <div>
+            <div>
+              <a href="#"><button>Twitter Link</button></a>
+            </div>
+            <div>
+              <a href="#"><span>Zuri Team</span></a>
+            </div>
+            <div>
+              <a href="#"><span>Zuri Books</span></a>
+            </div>
+            <div>
+              <a href="#"><span>Python books</span></a>
+            </div>
+            <div>
+              <a href="#"><span>Background Check For Coders</span></a>
+            </div>
+            <div>
+              <a href="#"><span>Design Books</span></a>
+            </div>
+            
+          </div>
+        </div>
+        <div>
+          {/* icons */}
+        </div>
+      </section>
+      <section>
+        <div>
+          {/* image */}
+          <p>HNG Internship 9 Frontend Task</p>
+          {/* image */}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
