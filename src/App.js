2a48b421baa3576c21937383cb38bfced56de814
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2022-10-31 01:02:03 +0100

    add css styles and made the design responsive

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-
+import profile from './assets/profile__img.svg'
+import icon from './assets/Icon.svg'
+import slack from './assets/slack.svg'
+import i4g from './assets/I4G.svg'
+import vector from './assets/Vector.svg'
+import returnD from './assets/returnD.svg'
+import returnM from './assets/returnM.svg'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <section>
-        <div>
-          <div>
-            <img alt=""/>
+    <div >
+      <section className='topContainer'>
+        <div className='topHeader'>
+          <div className='tHeader'>
+            <img src={profile} alt=""/>
             <h2>Annette black </h2>
-            {/* icon */}
+            <img src={returnD} className='tHeaderIcon' alt=""/>
           </div>
-          <div>
-            <div>
-              <a href="#"><button>Twitter Link</button></a>
-            </div>
-            <div>
-              <a href="#"><span>Zuri Team</span></a>
-            </div>
-            <div>
-              <a href="#"><span>Zuri Books</span></a>
-            </div>
-            <div>
-              <a href="#"><span>Python books</span></a>
-            </div>
-            <div>
-              <a href="#"><span>Background Check For Coders</span></a>
-            </div>
-            <div>
-              <a href="#"><span>Design Books</span></a>
-            </div>
+          <div className='tBody'>
+            
+              <a href="#" className=''><p className='p'>Twitter Link</p></a>
+            
+            
+              <a href="#"><p className='p'>Zuri Team</p></a>
+            
+              <a href="#"><p className='p'>Zuri Books</p></a>
+            
+              <a href="#"><p className='p'>Python books</p></a>
+            
+              <a href="#"><p className='p'>Background Check For Coders</p></a>
+           
+              <a href="#"><p className='p'>Design Books</p></a>
             
           </div>
         </div>
-        <div>
-          {/* icons */}
+        <div className='tFooter'>
+        <img src={slack} alt=""/>
+        <img src={icon} alt=""/>
         </div>
       </section>
-      <section>
-        <div>
-          {/* image */}
-          <p>HNG Internship 9 Frontend Task</p>
-          {/* image */}
+      <section className='bottomContainer'>
+        <div className='bBody'>
+          <div className='bContent'>
+            <img src={vector} className="zuri" alt=""/>
+            <p className='bottomP'>HNG Internship 9 Frontend Task</p>
+            <img src={i4g} className="hng" alt=""/>
+          </div>
         </div>
       </section>
     </div>
