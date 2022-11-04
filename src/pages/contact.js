9a450d0f8f0696d6fed32fc957955c1b8092eb3b
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2022-11-04 18:04:30 +0100

    feat: styled the textarea and checkbox with focus classes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,19 +8,6 @@ const Contact = () => {
         <div className='tContainer'>
           <div className='tHead'>
             <h2 className='t-h2'>Contact Me
-              
-              
-             
-              {/* This is a hint text to help user.
-              
-              This is a hint text to help user.
-              
-              
-              This is a hint text to help user.
-           
-              
-              
-              */}
             </h2>
             <p className='t-p'>Hi there, contact me to ask me about anything you have in mind.</p>
           </div>
@@ -44,10 +31,14 @@ const Contact = () => {
             </div>
             <div className='mInputs'>
               <label for=''>Message</label>
-              <textarea className='textInputs' type='text' rows={5} placeholder=' Send me a message and Ill reply you as soon as possible...' />
+              <textarea className='textInputs-tx' type='text' rows={5} placeholder=' Send me a message and Ill reply you as soon as possible...' />
+              <p className='e-p'>Please enter a message</p>
             </div>
             <div className='mInputs-cb'>
-              <input className='checkbox' type='checkbox' placeholder=' Enter your first name' />
+              <label className='checkbox'>
+                <input type='checkbox'/>
+                <span class="checkmark"></span>
+              </label>
               <p className='m-p'>You agree to providing your data to name who may contact you.</p>
             </div>
             <button className='div-btn'>
